67107c6b39bee9bd6b9c5a23cc1e4b72b4aa34cc
kao126 <76607249+kao126@users.noreply.github.com>
2022-08-01 14:52:28 +0000

    [Update]7c form部分テンプレート化

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,14 +17,22 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
+    @book = Book.new
   end
 
   def edit
     @group = Group.find(params[:id])
   end
 
-  private
+  def update
+    @group = Group.find(params[:id])
+    @group.owner_id = current_user.id
+    @group.update(group_params)
+    redirect_to group_path(@group)
+  end
 
+
+  private
   def group_params
     params.require(:group).permit(:name, :introduction, :group_image)
   end
